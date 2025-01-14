0bca9898a897c2ec4be5a6911c22458efae2ae52
nguyenphucthienan111 <155414257+nguyenphucthienan111@users.noreply.github.com>
2025-01-14 15:38:19 +0700

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
-import './Search.css'
-import SearchIcon from "@material-ui/icons/Search"
-import MicIcon from "@material-ui/icons/Mic"
-import KeyboardIcon from '@mui/icons-material/Keyboard';
-import FilterCenterFocusIcon from '@mui/icons-material/FilterCenterFocus';
-import { Button } from '@material-ui/core'
-
+import React, { useState } from "react";
+import "./Search.css";
+import SearchIcon from "@material-ui/icons/Search";
+import MicIcon from "@material-ui/icons/Mic";
+import KeyboardIcon from "@mui/icons-material/Keyboard";
+import FilterCenterFocusIcon from "@mui/icons-material/FilterCenterFocus";
+import { Button } from "@material-ui/core";
 
 function Search() {
   const [input, setInput] = useState("");
@@ -21,27 +20,19 @@ function Search() {
       <div className="search-input">
         <SearchIcon className="search-input-icon" />
         <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <KeyboardIcon />
         <MicIcon />
+        <FilterCenterFocusIcon />
       </div>
 
-    return (
-        <form className='search'>
-            <div className='search-input'>
-                <SearchIcon className="search-input-icon" />
-                <input value={input} onChange={(e) => setInput(e.target.value)} />
-                <KeyboardIcon />
-                <MicIcon />
-                <FilterCenterFocusIcon />
-
-
-            </div>
-
-            <div className='search-button'>
-                <Button type='submit' onClick={search} variant='outlined'>Google Search</Button>
-                <Button variant='outlined'>I'm feeling lucky</Button>
-            </div>
-        </form>
-    )
+      <div className="search-button">
+        <Button type="submit" onClick={search} variant="outlined">
+          Google Search
+        </Button>
+        <Button variant="outlined">I'm feeling lucky</Button>
+      </div>
+    </form>
+  );
 }
 
 export default Search;
