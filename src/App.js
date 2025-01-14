3e00d232e0cfc581885270f973adeb02fa9452af
nguyenphucthienan111 <155414257+nguyenphucthienan111@users.noreply.github.com>
2025-01-14 14:50:49 +0700

    update project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-
-import './App.css';
-import React from "react"
-import Home from './components/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import "./App.css";
+import React from "react";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-
           <Route path="/" element={<Home />} />
-
         </Routes>
       </BrowserRouter>
-
-
-
-
     </div>
   );
 }
