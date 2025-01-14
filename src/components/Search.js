b9287e047a3b7c4f6563be016da1df3caa96d0da
duypro974 <duypro974@gmail.com>
2025-01-14 16:27:25 +0700

    update keyboard

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,29 +5,46 @@ import MicIcon from "@material-ui/icons/Mic";
 import KeyboardIcon from "@mui/icons-material/Keyboard";
 import FilterCenterFocusIcon from "@mui/icons-material/FilterCenterFocus";
 import { Button } from "@material-ui/core";
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 
 function Search() {
   const [input, setInput] = useState("");
+  const [showKeyboard, setShowKeyboard] = useState(false);
 
   const search = (e) => {
     e.preventDefault();
-
     console.log("Search:", input);
+  };
+
+  const handleKeyboardInput = (value) => {
+    setInput(value); 
   };
 
   return (
     <form className="search">
       <div className="search-input">
         <SearchIcon className="search-input-icon" />
-        <input value={input} onChange={(e) => setInput(e.target.value)} />
-        <KeyboardIcon />
-        <div className="search-icon-wrapper" data-tooltip="Search by voice">
-          <MicIcon className="google-blue-icon" />
-        </div>
-        <div className="search-icon-wrapper" data-tooltip="Search by image">
-          <FilterCenterFocusIcon className="google-blue-icon" />
-        </div>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <KeyboardIcon
+          onClick={() => setShowKeyboard(!showKeyboard)}
+          style={{ cursor: "pointer" }}
+        />
+        <MicIcon />
+        <FilterCenterFocusIcon />
       </div>
+
+      {showKeyboard && ( 
+        <div className="virtual-keyboard">
+          <Keyboard
+            onChange={handleKeyboardInput}
+            inputName={"searchInput"} 
+          />
+        </div>
+      )}
 
       <div className="search-button">
         <Button type="submit" onClick={search} variant="outlined">
