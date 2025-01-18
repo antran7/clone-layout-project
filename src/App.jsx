bc8cc98148166089dad694111298334b807093c4
nguyenphucthienan111 <155414257+nguyenphucthienan111@users.noreply.github.com>
2025-01-18 20:50:33 +0700

    update sidebar youtube

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import Home from "./components/Home";
 import Youtube from "./components/YouTube/Youtube";
 import Shorts from "./components/YouTube/Shorts";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/google" replace />} />
           <Route path="/google" element={<Home />} />
           <Route path="/youtube" element={<Youtube />} />
           <Route path="/shorts" element={<Shorts />} />
