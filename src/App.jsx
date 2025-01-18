8a55baaf55b9ffa40700c71e5b76021fd99137d8
nguyenphucthienan111 <155414257+nguyenphucthienan111@users.noreply.github.com>
2025-01-18 20:58:55 +0700

    Reapply "Reapply "Merge branch 'youtube'""
    This reverts commit cf35589f7371370d6e402f511314c98f88e33db8.

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import Home from "./components/Home";
 import Youtube from "./components/YouTube/Youtube";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Gmail from "./components/Gmail/Gmail";
+import Shorts from "./components/YouTube/Shorts";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/google" replace />} />
           <Route path="/google" element={<Home />} />
           <Route path="/youtube" element={<Youtube />} />
           <Route path="/gmail" element={<Gmail />} />
+          <Route path="/shorts" element={<Shorts />} />
         </Routes>
       </BrowserRouter>
     </div>
