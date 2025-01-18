cbfcac4a7e94a337d2b3b3d891984916a478b49b
nguyenphucthienan111 <155414257+nguyenphucthienan111@users.noreply.github.com>
2025-01-18 22:31:03 +0700

    update youtube

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import "./App.css";
 import React from "react";
 import Home from "./components/Home";
 import Youtube from "./components/YouTube/Youtube";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shorts from "./components/YouTube/Shorts";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Gmail from "./components/Gmail/Gmail";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/google" replace />} />
           <Route path="/google" element={<Home />} />
           <Route path="/youtube" element={<Youtube />} />
+          <Route path="/shorts" element={<Shorts />} />
           <Route path="/gmail" element={<Gmail />} />
         </Routes>
       </BrowserRouter>
